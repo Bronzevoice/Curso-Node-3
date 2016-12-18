cfa60c20f7d25685c4188b1079c5323b7844b305
BronzeVoice <danielmonlpz@gmail.com>
2016-12-18 23:13:20 +0100

    second commit

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express'),swig = require('swig');
+var server = express();
+
+
+//Config Swig
+server.engine('html', swig.renderFile);
+server.set('view engine', 'html');
+server.set('views', __dirname + '\\app\\views');
+
+server.use(express.static('./public'));
+
+require('./app/controllers/home')(server);
+
+server.listen(8000);
